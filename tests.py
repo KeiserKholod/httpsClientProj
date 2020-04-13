f50bb04cced4ec61da62f06c0f2f0ad2e692457b
Dimitry <Kholod.ural@gmail.com>
2020-04-13 23:36:32 +0500

    added tests for work with files

--- tests.py
+++ tests.py
@@ -87,6 +87,18 @@ class TestRequest(unittest.TestCase):
         request.do_request()
         self.assertEqual(req, request.request_to_send)
 
+    def test_cookie_file(self):
+        cmd_parser = http_client.create_cmd_parser()
+        args = cmd_parser.parse_args(['http://ptsv2.com/t/xfg/post', '--cookie-file', 'cookie.txt'])
+        request = http_client.Request(args)
+        self.assertEqual('qwer=ty', request.cookie)
+
+    def test_body_file(self):
+        cmd_parser = http_client.create_cmd_parser()
+        args = cmd_parser.parse_args(['http://ptsv2.com/t/xfg/post', '--body-file', 'body.txt'])
+        request = http_client.Request(args)
+        self.assertEqual('pass=word', request.data_to_send)
+
     def test_get_with_args(self):
         req = 'GET /t/xfg/post?qw=12 HTTP/1.1\r\n' \
               'Host: ptsv2.com\r\n' \
@@ -96,48 +108,6 @@ class TestRequest(unittest.TestCase):
         request = http_client.Request(args)
         request.do_request()
         self.assertEqual(req, request.request_to_send)
-
-
-class TestResponse(unittest.TestCase):
-    headers = b'HTTP/1.1 200 OK\r\n' \
-              b'Access-Control-Allow-Origin: *\r\n' \
-              b'Content-Type: text/html; charset=utf-8\r\n' \
-              b'X-Cloud-Trace-Context: 22097699704b3f0712a88db1c88d3974\r\n' \
-              b'Date: Sat, 11 Apr 2020 14:05:46 GMT\r\n' \
-              b'Server: Google Frontend\r\n' \
-              b'Content-Length: 54\r\n' \
-              b'Connection: close\r\n\r\n'
-    body = b'Thank you for this dump. I hope you have a lovely day!'
-
-    def test_init_resp(self):
-        resp_txt = TestResponse.headers + TestResponse.body
-        resp = http_client.Response(resp_txt)
-        self.assertEqual(TestResponse.headers, resp.headers)
-        self.assertEqual(TestResponse.body, resp.body)
-
-    def test_prepare_resp_head(self):
-        resp_txt = TestResponse.headers + TestResponse.body
-        cmd_parser = http_client.create_cmd_parser()
-        args = cmd_parser.parse_args(['https://abc.de:123/request', '-0'])
-        resp = http_client.Response(resp_txt)
-        resp.prepare_response(args)
-        self.assertEqual(resp.response_to_print, TestResponse.headers)
-
-    def test_prepare_resp_body(self):
-        resp_txt = TestResponse.headers + TestResponse.body
-        cmd_parser = http_client.create_cmd_parser()
-        args = cmd_parser.parse_args(['https://abc.de:123/request', '-1'])
-        resp = http_client.Response(resp_txt)
-        resp.prepare_response(args)
-        self.assertEqual(resp.response_to_print, TestResponse.body)
-
-    def test_prepare_resp_all(self):
-        resp_txt = TestResponse.headers + TestResponse.body
-        cmd_parser = http_client.create_cmd_parser()
-        args = cmd_parser.parse_args(['https://abc.de:123/request', '-2'])
-        resp = http_client.Response(resp_txt)
-        resp.prepare_response(args)
-        self.assertEqual(resp.response_to_print, resp_txt)
 
 
 class TestResponse(unittest.TestCase):
